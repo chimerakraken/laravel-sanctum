86c76c43176d0a5b9a04d799c27415aa6dd16161
unknown <johnnyrufinojr@gmail.com>
2021-03-24 14:56:03 +0800

    Initial commit laravel-sanctum

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+
+Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
